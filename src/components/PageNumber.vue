d43767e2ac0897c550e042fbbb485da681f802ac
alucky36572 <alucky36572@gmail.com>
2024-01-23 16:40:36 +0800

    新增頁碼及卡片元件

---
+++ src/components/PageNumber.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="PageNumber">
+        <a @click.prevent='changePage(false)' :class="{ disabled: currentPage === 1 }" id="prevpage"><i class="fa-solid fa-chevron-left" style="color: #e73f14;"></i></a>
+        <a @click.prevent='changePage(item)'  :class='{ active: currentPage === item }' v-for='item in list'
+            :key='item'>{{ item }}</a>
+        <a @click.prevent='changePage(true)' :class='{ disabled: currentPage === pages }' id="nextpage"><i class="fa-solid fa-chevron-right" style="color: #e73f14;"></i></a>
+    </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+    name: 'PageNumber',
+    props: {
+        total: {
+            type: Number,
+            default: 45
+        },
+        pagesize: {
+            type: Number,
+            default: 9
+        }
+    },
+    setup(props, { emit, attrs }) {
+        const pages = computed(() => Math.ceil(props.total / props.pagesize))
+        // 當前頁碼
+        const currentPage = ref(attrs.page || 1)
+        // 動態計算頁碼
+        const list = computed(() => {
+            // 當父元件傳遞total的值改變時，計算屬性會重新計算
+            // pages = Math.ceil(props.total / props.pagesize)
+            const result = []
+            if (pages.value <= 5) {
+                for (let i = 1; i <= pages.value; i++) {
+                    result.push(i)
+                }
+            } else {
+                // 總頁碼大於5
+                if (currentPage.value <= 2) {
+                    // 左側臨界值
+                    for (let i = 1; i <= 5; i++) {
+                        result.push(i)
+                    }
+                } else if (currentPage.value >= pages.value - 1) {
+                    // 右側
+                    for (let i = pages.value - 4; i <= pages.value; i++) {
+                        result.push(i)
+                    }
+                } else {
+                    // 中間
+                    for (let i = currentPage.value - 2; i <= currentPage.value + 2; i++) {
+                        result.push(i)
+                    }
+                }
+            }
+            return result
+        })
+        // 控制上一頁和下一頁變化
+        const changePage = (type) => {
+            if (type === false) {
+                // 上一頁
+                // 頁面是第一頁時，禁止點擊操作
+                if (currentPage.value === 1) return
+                if (currentPage.value > 1) {
+                    currentPage.value -= 1
+                }
+            } else if (type === true) {
+                // 下一頁
+                if (currentPage.value === pages.value) return
+                if (currentPage.value < pages.value) {
+                    currentPage.value += 1
+                }
+            } else {
+                // 點選頁碼
+                currentPage.value = type
+            }
+            emit('change-page', currentPage.value)
+        }
+        return { list, currentPage, pages, changePage }
+
+    }
+}
+</script>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -9,43 +9,54 @@
     </div>
     <breadcrumb :breadcrumb="yourBreadcrumbData"></breadcrumb>
     <div class="product-list container">
-      <aside>
+      <ul>
         <li>
-          <div class="lunchbox">調理包</div>
+          <div class="lunchbox" @click="filter(0, 'lunchbox')">調理包</div>
           <hr>
         </li>
         <li>
-          <div class="freshfood" @click="toggle = !toggle">
-          <h3>生鮮食材</h3> 
-          <i class="fa-solid fa-angle-down" style="color: #e73f14;"></i>
+          <div class="freshfood" @click="toggle = !toggle; filter(5, 'freshfood')">
+            <h3>生鮮食材</h3>
+            <i class="fa-solid fa-angle-down" style="color: #e73f14;"></i>
           </div>
           <hr>
-          <div class="oil" v-if="!toggle">雜糧、油品</div>
-          <div class="egg" v-if="!toggle">蛋、豆</div>
-          <div class="fish" v-if="!toggle">魚、肉</div>
-          <div class="vegetable" v-if="!toggle">蔬菜</div>
+          <div class="oil" v-if="!toggle" @click="filter(1, 'oil')">雜糧、油品</div>
+          <div class="egg" v-if="!toggle" @click="filter(2, 'egg')">蛋、豆</div>
+          <div class="fish" v-if="!toggle" @click="filter(3, 'fish')">魚、肉</div>
+          <div class="vegetable" v-if="!toggle" @click="filter(4, 'vegetable')">蔬菜</div>
           <hr v-if="!toggle">
         </li>
-      </aside>
-      <div class="row items">
+      </ul>
 
-        <div v-for="item in displayData" :key="item.id" class="vegetable_card col-12 col-xl-3">
-
-          <p class="product_tag">#NEW</p>
-          <div class="product_card_img">
-            <!-- <img :src="getImage(item)" height="200"> -->
-            <img src="../../src/assets/images/product/vegetable_cover.png" alt="vegetable_cover">
+      <div class="phoneList">
+        <select @change="filterPhoneList">
+          <option value="">所有商品</option>
+          <option value="lunchbox">調理包</option>
+          <option value='oil'>雜糧、油品</option>
+          <option value='egg'>蛋、豆</option>
+          <option value='fish'>魚、肉</option>
+          <option value='vegetable'>蔬菜</option>
+        </select>
+      </div>
+      <div class="row product_items">
+        <template v-for="outerIndex in 9" :key="outerIndex">
+          <div v-for="(item, innerIndex) in productDisplay" :key="`${outerIndex}-${innerIndex}`"
+            class="col-12 col-md-3 vegetable_card">
+            <p class="product_tag">#NEW</p>
+            <div class="product_card_img">
+              <img :src="item.image" alt="item.name">
+            </div>
+            <p class="vegetable_title">{{ item.name }}</p>
+            <p class="vegetable_price">{{ item.price }}</p>
+            <router-link to="/productinfo" class="btn-product">查看商品詳情</router-link>
           </div>
-          <p class="vegetable_title">冷凍蔬菜</p>
-          <p class="vegetable_price">$ 330</p>
-          <router-link to="/productinfo" class="btn-product">查看商品詳情</router-link>
-
-        </div>
-        <p class="vegetable_title">{{ item.prod_name }}</p>
-        <p class="vegetable_price">$ {{ item.prod_sellingprice }}</p>
-        <router-link to="/productinfo" class="btn-product">查看商品詳情</router-link>
+        </template>
       </div>
     </div>
+
+    <div class="home-container">
+   <PageNumber @change-page='changePage' :pagesize='9' :total='45' :page='1' />
+ </div>
 
   </div>
 </template>
@@ -54,6 +65,10 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Breadcrumb from '@/components/Breadcrumb.vue';
 import axios from 'axios';
+import PageNumber from '@/components/PageNumber.vue';
+
+import { reactive } from 'vue'
+
 
 export default {
   data() {
@@ -63,8 +78,46 @@ export default {
         { text: '首頁', to: '/' },
         { text: '健康小舖', active: true },
       ],
-      displayData: [],
+      displayData: [
+        {
+          index: 4,
+          name: "冷凍蔬菜",
+          price: "$330",
+          image: "../../src/assets/images/product/vegetable_cover.png",
+          type: "vegetable"
+        },
+        {
+          index: 0,
+          name: "南瓜蔬食調理包",
+          price: "$170",
+          image: "../../src/assets/images/product/pumpkin_cover.png",
+          type: "lunchbox"
+        },
+        {
+          index: 2,
+          name: "有機雞蛋",
+          price: "$100",
+          image: "../../src/assets/images/product/eggs-cover.png",
+          type: "egg"
+        },
+        {
+          index: 1,
+          name: "食用油",
+          price: "$300",
+          image: "../../src/assets/images/product/oil-cover.png",
+          type: "oil"
+        },
+        {
+          index: 3,
+          name: "水產養殖鮮魚",
+          price: "$500",
+          image: "../../src/assets/images/product/fish-cover.png",
+          type: "fish"
+        },
+      ],
       toggle: true,
+      productDisplay: []
+
 
     };
   },
@@ -72,26 +125,76 @@ export default {
     RouterLink,
     RouterView,
     Breadcrumb,
-  },
+    PageNumber
+},
   created() {
-    this.axiosGetData()
+    // this.axiosGetData();
+    this.productDisplay = this.displayData;
+
   },
-  
+  setup () {
+    // 篩選條件準備
+    const reqParams = reactive({
+      // 目前頁碼
+      page: 1,
+      // 每頁的數量
+      pageSize: 9
+    })
+    // 控制頁碼的變化
+    const changePage = (page) => {
+      // console.log(page)
+      reqParams.page = page
+    }
+ 
+    return { changePage }
+  },
+
+
   methods: {
     axiosGetData() {
-      axios.get('https://tibamef2e.com/chd103/g1/phps/shop.php')
-        .then(res => {
-          if (res && res.data) {
-            // this.responseData = res.data
-            this.displayData = res.data
-          }
-        })
+      // axios.get('https://tibamef2e.com/chd103/g1/phps/shop.php')
+
+      //   .then(res => {
+      //     if (res && res.data) {
+      //       // this.responseData = res.data
+      //       this.displayData = res.data
+      //     }
+      //   })
     },
     // getImage(item) {
     //   return `https://tibamef2e.com/chd103/g1/image/productimage/${item.prod_img}`;
     // },
+    filterPhoneList(e) {
+      const filterPhoneType = e.target.value;
 
+      if (!filterPhoneType || filterPhoneType === '') {
+        // 如果選擇的是空字符串或 undefined，顯示所有商品
+        this.productDisplay = this.displayData;
+      } else if (['lunchbox', 'oil', 'egg', 'fish', 'vegetable'].includes(filterPhoneType)) {
+        // 否則，進行其他商品類型的篩選
+        this.productDisplay = this.displayData.filter(item => {
+          return item.type === filterPhoneType;
+        });
+      }
+    },
+  
+
+
+  filter(filterIndex, filterType) {
+    this.change = filterIndex
+    //篩選商品:透過陣列filter方法篩選物件中type相同的商品
+    if (filterType === 'freshfood') {
+      this.productDisplay = this.displayData.filter(item => {
+        return item.index >= 1 && item.index <= 4;
+      })
+    } else {
+      this.productDisplay = this.displayData.filter(item => {
+        // console.log(this.productDisplay.length);
+        return item.index === filterIndex
+      })
+    }
   },
 
+}
 }
 </script>
