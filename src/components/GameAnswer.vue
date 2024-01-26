adbf68f91998a92046ace4ac11f84d8f1170cde4
fafamia <miapyu88@gmail.com>
2024-01-26 09:14:21 +0800

    新增健康問答遊戲

---
+++ src/components/GameAnswer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="game_a row">
+        <div class="game_a_img col-12 col-xl-6">
+            <img :src="image" :alt="altText" />
+        </div>
+        <div class="game_a_text col-12 col-xl-6">
+            <h2 class="game_a_title">{{ title }}</h2>
+            <p>{{ explanation }}</p>
+            <button type="button" class="btn-product" @click="nextQuestion">
+                {{ isLastQuestion ? '挑戰完成' : '下一題' }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GameAnswer',
+    props: {
+        image: String,
+        altText: String,
+        title: String,
+        explanation: String,
+        isLastQuestion: Boolean,
+    },
+    emits: ['next-question'],
+    methods: {
+        nextQuestion() {
+            // 发出 next-question 事件
+            this.$emit('next-question');
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/page/_game.scss";
+</style>
---
+++ src/components/GameQuestion.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="game_q">
+        <p class="game_q_num">{{ questionNum }}</p>
+        <h2 class="game_q_topic">{{ questionText }}</h2>
+        <ol class="game_q_options">
+            <li v-for="(option, index) in options" :key="index" @click="selectOption(option)">
+                {{ option.text }}
+            </li>
+        </ol>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'GameQuestion',
+    props: {
+        questionNum: String,
+        questionText: String,
+        options: Array
+    },
+    methods: {
+        selectOption(option) {
+            // 发出选项被选中的事件
+            this.$emit('option-selected', option);
+        }
+    }
+};
+</script>
+  
+<style lang="scss">
+@import "@/assets/scss/page/_game.scss";
+</style>
