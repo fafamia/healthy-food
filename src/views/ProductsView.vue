45ab67dd1a5516d05637200682019c9152bc3283
MMMMMilo <helloapple0321@gmail.com>
2024-03-01 14:49:15 +0800

    分支檔案追最新dev

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -23,22 +23,22 @@
         </select>
       </div>
       <div class="row product_items">
-        <div v-for="(item, index) in displayList" :key="item.product_no" class="col-12 col-md-3 vegetable_card">
-          <p class="product_tag">#{{ item.product_tag_name }}</p>
-          <div class="product_card_img">
-            <img :src=getImageUrl(item.product_img) :alt="item.product_name" class="product_image">
-            <button class="heart" @click="keepProd(item)" :title="item.heartFilled ? '取消收藏' : '收藏商品'">
-              <i :class="{ 'fa-regular': !item.heartFilled, 'fa-solid': item.heartFilled, 'fa-heart': true }"
-                style="color: #f50a0a;"></i>
-            </button>
+          <div v-for="(item, index) in displayList" :key="item.product_no" class="col-12 col-md-3 vegetable_card">
+            <p class="product_tag">#{{ item.product_tag_name }}</p>
+            <div class="product_card_img">
+              <img :src=getImageUrl(item.product_img) :alt="item.product_name" class="product_image">
+              <button class="heart" @click="keepProd(item)" :title="item.heartFilled ? '取消收藏' : '收藏商品'">
+                <i :class="{ 'fa-regular': !item.heartFilled, 'fa-solid': item.heartFilled, 'fa-heart': true }"
+                  style="color: #f50a0a;"></i>
+              </button>
+            </div>
+            <p class="vegetable_title">{{ item.product_name }}</p>
+            <p class="vegetable_price">${{ item.product_price }}</p>
+            <router-link :to="{
+              name: 'productinfo',
+              params: { product_no: item.product_no }
+            }" class="btn-product">查看商品詳情</router-link>
           </div>
-          <p class="vegetable_title">{{ item.product_name }}</p>
-          <p class="vegetable_price">${{ item.product_price }}</p>
-          <router-link :to="{
-            name: 'productinfo',
-            params: { product_no: item.product_no }
-          }" class="btn-product">查看商品詳情</router-link>
-        </div>
         <div class="home-container">
           <PageNumber @change-page='changePage' :pagesize="reqParams.pageSize" :total='productDisplay.length'
             :page="reqParams.page" />
@@ -53,7 +53,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import Breadcrumb from '@/components/Breadcrumb.vue';
 // import heart from '@/components/heart.vue';
 import PageNumber from '@/components/PageNumber.vue';
-import { reactive, ref, onMounted, computed } from 'vue'
+import { reactive, ref, onMounted, computed,watch } from 'vue'
 import { useProductStore } from '@/stores/Product';
 import { userStore } from '@/stores/user';
 
@@ -76,13 +76,18 @@ export default {
     const ProductStore = useProductStore();
     const originData = ref([]);
     const productDisplay = ref([]);
+    // const productPage = ref([]);
+    
     const productClass = ref([]);
     //要在vue模板編譯後引入，如果直接放在setup中會比pinia快
     onMounted(async () => {
       await ProductStore.getProductData();
       await ProductStore.getProductClassData();
       originData.value = ProductStore.products;
-      productDisplay.value = originData.value;
+      productDisplay.value = ProductStore.products;
+
+      // productPage.value = ProductStore.products;
+      
       productClass.value = ProductStore.productClass;
       productDisplay.value.forEach(item => {
         if (favoriteProducts.value.includes(item.product_no)) {
@@ -96,9 +101,9 @@ export default {
       if (classNo !== 0) {
         //database:sql->int, serve-side:php->string(jaon response), clint-side:vue(HTML-JS)->string(select-option)
         //參數在不同地方轉傳容易有型別不一樣的問題，統一型別再比較或是用 == 比較
-        productDisplay.value = originData.value.filter(item => `${item.product_class_no}` === `${classNo}`);
+        productDisplay.value = originData.value.filter(item => `${item.product_class_no}` == `${classNo}`);
       }else{
-        productDisplay.value = originData.value;
+        productDisplay.value;
       }
     };
     const filterPhoneList = (e) => {
@@ -117,8 +122,9 @@ export default {
     });
     // 控制頁碼的變化
     const changePage = (page) => {
-      reqParams.page = page
+      reqParams.page = 1;
       scrollToTop(); // 新增：在換頁碼時滾動到最上面
+      // filter(classNo);
     }
     // 滾動到最上面的方法
     const scrollToTop = () => {
